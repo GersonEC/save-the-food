22791cb26038b264794dc4255d317ec21bfd9be3
GersonEC <gersonenriquez94@gmail.com>
2025-07-15 09:39:04 +0200

    chore: fix build

--- src/components/BadgeCategory/index.tsx
+++ src/components/BadgeCategory/index.tsx
@@ -7,16 +7,16 @@ interface BadgeCategoryProps {
 }
 
 export const BadgeCategory = ({ category }: BadgeCategoryProps) => {
-  const mapCategoryClassName = {
-    Fruits: 'bg-green-600/30 text-green-700',
-    Vegetables: 'bg-blue-600/30 text-blue-700',
-    Meat: 'bg-red-600/30 text-red-700',
-    Fish: 'bg-yellow-600/30 text-yellow-700',
-    Grains: 'bg-purple-600/30 text-purple-700',
-    Beverages: 'bg-orange-600/30 text-orange-700',
-    Snacks: 'bg-pink-600/30 text-pink-700',
-    Other: 'bg-gray-600/30 text-gray-700',
-    Leftovers: 'bg-gray-600/30 text-gray-700',
+  const mapCategoryClassName: Record<FoodCategory, string> = {
+    Frutta: 'bg-green-600/30 text-green-700',
+    Verdura: 'bg-blue-600/30 text-blue-700',
+    Carne: 'bg-red-600/30 text-red-700',
+    Pesce: 'bg-yellow-600/30 text-yellow-700',
+    Grano: 'bg-purple-600/30 text-purple-700',
+    Bevande: 'bg-orange-600/30 text-orange-700',
+    Snack: 'bg-pink-600/30 text-pink-700',
+    Avanzi: 'bg-gray-600/30 text-gray-700',
+    Altro: 'bg-gray-600/30 text-gray-700',
   };
 
   return (
